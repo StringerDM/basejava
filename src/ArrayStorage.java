d87fbcbab7c6baea62232ed4b67e50b95954adf1
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-05-13 13:26:46 +0300

    HW1 with corrections 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -31,35 +31,25 @@ public class ArrayStorage {
     void delete(String uuid) {
         if (size > 0) {
             boolean isRemoved = false;
+            int removedIndex = 0;
             for (int i = 0; i < size; i++) {
                 if (storage[i].uuid.equals(uuid)) {
-                    storage[i] = null;
                     isRemoved = true;
-                }
-                if (isRemoved) {
-                    if (i < size - 1) {
-                        storage[i] = storage[i + 1];
-                    } else {
-                        storage[i] = null;
-                    }
+                    removedIndex = i;
                 }
             }
             if(isRemoved) {
+                System.arraycopy(storage, removedIndex + 1, storage, removedIndex, size - removedIndex);
                 size--;
             }
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        if (size != 0) {
-            return Arrays.copyOf(storage, size);
-        } else {
-            return new Resume[0];
-        }
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
