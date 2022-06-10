b320357c5dab667668d8edc06d3311f0f245b1e2
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-06-10 21:54:42 +0300

    HW11 correction 1

--- src/ru/javaops/webapp/MainConcurrency.java
+++ src/ru/javaops/webapp/MainConcurrency.java
@@ -1,31 +1,30 @@
 package ru.javaops.webapp;
 
 public class MainConcurrency {
-    static final Object LOCK1 = new Object();
-    static final Object LOCK2 = new Object();
+    static final String LOCK1 = "LOCK1";
+    static final String LOCK2 = "LOCK2";
 
     public static void main(String[] args) {
+        deadlock(LOCK1, LOCK2);
+        deadlock(LOCK2, LOCK1);
+    }
 
+    public static void deadlock(Object obj1, Object obj2) {
         new Thread(() -> {
-            synchronized (LOCK1) {
+            System.out.println(Thread.currentThread().getName() + " started");
+            System.out.println(Thread.currentThread().getName() + " trying to synchronize by " + obj1);
+            synchronized (obj1) {
+                System.out.println(Thread.currentThread().getName() + " synchronized by " + obj1);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                synchronized (LOCK2) {
-                    System.out.println(Thread.currentThread().getName());
+                System.out.println(Thread.currentThread().getName() + " trying to synchronize by " + obj2);
+                synchronized (obj2) {
+                    System.out.println(Thread.currentThread().getName() + " synchronized by " + obj2);
                 }
             }
-        }).start();
-
-        new Thread(() -> {
-            synchronized (LOCK2) {
-                synchronized (LOCK1) {
-                    System.out.println(Thread.currentThread().getName());
-                }
-            }
-
         }).start();
     }
 }
