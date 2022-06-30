390332f570a19c7632ded79a7e8e13de6cd0f4b1
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-06-30 10:41:45 +0300

    Apply Lesson15 commits

---
+++ src/ru/javaops/webapp/web/ResumeServlet.java
@@ -0,0 +1,22 @@
+package ru.javaops.webapp.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+    }
+}
