975ee524085558ae7b9d9764cc3b0f3300358fd5
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-07-27 17:39:32 +0300

    Lesson17 DB_credentials_as_resources final basejava commit

--- src/ru/javaops/webapp/Config.java
+++ src/ru/javaops/webapp/Config.java
@@ -10,14 +10,14 @@ import java.nio.file.Files;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File(getHomeDir(), "\\config\\resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
     private final Storage storage;
 
     private Config() {
-        try (InputStream is = Files.newInputStream(PROPS.toPath())) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
@@ -25,7 +25,7 @@ public class Config {
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
 
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -43,12 +43,12 @@ public class Config {
         return storage;
     }
 
-    private static File getHomeDir() {
-        String prop = System.getProperty("homeDir");
-        File homeDir = new File(prop == null ? "." : prop);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir + " is not directory");
-        }
-        return homeDir;
-    }
+//    private static File getHomeDir() {
+//        String prop = System.getProperty("homeDir");
+//        File homeDir = new File(prop == null ? "." : prop);
+//        if (!homeDir.isDirectory()) {
+//            throw new IllegalStateException(homeDir + " is not directory");
+//        }
+//        return homeDir;
+//    }
 }
